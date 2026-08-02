6ab5e9c55e58d252fcf1c9935dab488973eee75c
metalcoat <crfrary@gmail.com>
2026-08-02 18:37:01 -0400

    Remove duplicate operations navigation

--- src/app/ops/layout.tsx
+++ src/app/ops/layout.tsx
@@ -4,13 +4,5 @@ import "./ops.css";
 export default function OperationsLayout({
   children,
 }: Readonly<{ children: ReactNode }>) {
-  return <>
-    <div className="opsUtilityNav">
-      <a href="/ops">Operations</a>
-      <a href="/ops/integrations">Scheduler & Integrations</a>
-      <a href="/ops/bank-accounts">Bank Accounts</a>
-      <a href="/">Documents</a>
-    </div>
-    {children}
-  </>;
+  return children;
 }
