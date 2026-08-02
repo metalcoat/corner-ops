e60236a4d90a902d46d3848cc69ecce19e337ebe
metalcoat <crfrary@gmail.com>
2026-08-02 18:23:44 -0400

    Add bank account selector to operations navigation

--- src/app/ops/layout.tsx
+++ src/app/ops/layout.tsx
@@ -8,6 +8,7 @@ export default function OperationsLayout({
     <div className="opsUtilityNav">
       <a href="/ops">Operations</a>
       <a href="/ops/integrations">Scheduler & Integrations</a>
+      <a href="/ops/bank-accounts">Bank Accounts</a>
       <a href="/">Documents</a>
     </div>
     {children}
